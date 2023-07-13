1ef01ea2c447a2ff747271f17064327bbca3a254
Fahmiawaludin <fahmi.123awaludin@gmail.com>
2023-07-13 21:48:19 +0700

    jadi jam tayang

--- assets/js/main.js
+++ assets/js/main.js
@@ -291,3 +291,22 @@
   new PureCounter();
 
 })()
+
+
+const count = document.getElementById("count");
+
+incrementVisitsCount();
+
+function incrementVisitsCount() {
+    let visits;
+
+    if (!localStorage.getItem("visits")) localStorage.setItem("visits", 1);
+    else {
+        visits = +localStorage.getItem("visits");
+        const incrementedCount = visits + 1;
+
+        localStorage.setItem("visits", incrementedCount);
+    }
+
+    count.innerText = localStorage.getItem("visits");
+}
